a88d088bdbfae8546f30cb94ce078055867a59dd
Alisson Grizotti <alisson1397@hotmail.com>
2019-02-06 22:31:50 -0200

    Update init.mmi.boot.sh
    Mensagem de Troca de Chip ao reiniciar corrigido.

--- rootdir/etc/init.mmi.boot.sh
+++ rootdir/etc/init.mmi.boot.sh
@@ -46,7 +46,4 @@ fi
 insmod /vendor/lib/modules/isdbt.ko
 
 
-if [ -f /data/system/users/0/settings_global.xml ]; then
-    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g'  /data/system/users/0/settings_global.xml
-    restorecon /data/system/users/0/settings_global.xml
-fi
+
